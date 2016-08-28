274347989d1e745b4c5c0a055213d19b6009fe44
Brent Taylor <btaylor@fuzzylogicstudios.com>
2016-08-27 19:41:38 -0700

    Move functionality into packages.el
    Extensions.el convention is deprecated.

--- extensions.el
+++
@@ -1,4 +0,0 @@
-(setq godot-pre-extensions '(gdscript))
-
-(defun godot/init-gdscript ()
-  (use-package gdscript))
---
+++ packages.el
@@ -0,0 +1,5 @@
+(setq godot-packages '(
+                       (gdscript :location local)))
+
+(defun godot/init-gdscript ()
+  (use-package gdscript))
